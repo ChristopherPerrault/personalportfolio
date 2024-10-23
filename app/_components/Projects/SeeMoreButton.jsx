44f6824ec94624ebaa39f7b5417ffc92722aef8a
Chris <christopherperrault@hotmail.com>
2024-10-23 15:41:44 -0400

    minor style edit

--- app/_components/Projects/SeeMoreButton.jsx
+++ app/_components/Projects/SeeMoreButton.jsx
@@ -5,14 +5,16 @@ import { useTranslation } from "react-i18next";
 export default function SeeMoreButton({ onClick }) {
   const { t } = useTranslation();
 
+  const buttonStyles =
+    "w-full px-5 py-2 text-lg font-semibold transition-transform duration-200 transform shadow-md rounded-md bg-white hover:bg-yellow-500";
+
   return (
-    <div className="mt-8 text-center">
-      <button
-        onClick={onClick}
-        className="px-8 py-3 text-lg text-black transition-colors duration-300 bg-yellow-500 border-2 border-black rounded-full hover:bg-yellow-600 hover:border-yellow-600"
-      >
-        {t("project.seeMoreBtn")}
-      </button>
+    <div className="flex justify-center my-4">
+      <div className="flex rounded-md p-0.5 bg-black">
+        <button onClick={onClick} className={buttonStyles}>
+          {t("project.seeMoreBtn")}
+        </button>
+      </div>
     </div>
   );
 }
